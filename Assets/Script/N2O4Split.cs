1f44d03254747ffd0ef76af940645e1c205a2a5f
Brandon Shin <bmshin02@gmail.com>
2023-06-07 13:17:23 -0700

    Fixed 'no instance' error

--- Assets/Script/N2O4Split.cs
+++ Assets/Script/N2O4Split.cs
@@ -10,28 +10,19 @@ public class N2O4Split : MonoBehaviour
 
     public GameObject particleGen;
     float timer = 0f;
-    private int listSize;
+    private int listSize = 0;
     private List<GameObject> currList = null;
 
-    void Start()
-    {
 
-        listSize = 0;
-        timer = 0f;
-        currList = ParticleGeneration.N2O4List;
-        //currList = particleGen.GetComponent<ParticleGeneration>().GetN2O4List();
-    }
-    // Start is called before the first frame update
     void Update()
     {
-
-        
-        listSize = currList.Count;
-        timer += Time.deltaTime;
+        //currList = particleGen.GetComponent<ParticleGeneration>().GetN2O4List();
+        //listSize = currList.Count;
+        //timer += Time.deltaTime;
 
         if (timer > 5f && listSize > 0)
         {
-            // Debug.Log("5 SECONDS PASSED. About to delete");
+            Debug.Log("5 SECONDS PASSED. About to delete");
             //particleGen.GetComponent<ParticleGeneration>().DestroyGameObjects("N2O4");
             timer = 0f;
         }
